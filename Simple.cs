686435860e740a62f3cc27321dcc3f7450b2baea
Miguel <118042356+MigzAndroidz@users.noreply.github.com>
2023-08-09 22:09:18 +0800

    Create Simple.cs

---
+++ Simple.cs
@@ -0,0 +1,13 @@
+namespace Try
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Miguel", name2 = "Janelle", name3 = "Hi";
+            int age1 = 18, age2 = 18, age3 = 19;
+            double grade1 = 89, grade2 = 98, grade3 = 76;
+            Console.WriteLine("Janelle is smart because her grade is " + grade2);
+        }
+    }
+}
